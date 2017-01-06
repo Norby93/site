c96315dddd8b9c51eb0de070fef20fe80fbcea8e
Norby93 <latko2000@yandex.ru>
2017-01-06 15:26:30 +0200

    1st branch

--- 123.php
+++ 123.php
@@ -1,16 +1,10 @@
 <?php // upload.php
 echo <<<_END
-<html><head><title>PHP-форма для загрузки файлов на сервер</title></head><body>
-<form method='post' action='upload.php' enctype='multipart/form-data'>
-Выберите файл: <input type='file' name='filename' size='10'>
-<input type='submit' value='Загрузить'>
-</form>
+xcgh1
+dfgh12
+dfgh12
+dfghd12
+fhg1
+dfgh12
 _END;
-if ($_FILES)
-{
-    $name = $_FILES['filename']['name'];
-    move_uploaded_file($_FILES['filename']['tmp_name'], $name);
-    echo "Загружаемое изображение '$name'<br><img src='$name'>";
-}
-echo "</body></html>";
 ?>
